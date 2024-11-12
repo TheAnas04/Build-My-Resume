8e2ef032228c07368cab1bfef23a78525db78726
TheAnas04 <04anasshaikh04@gmail.com>
2024-11-12 16:48:02 +0530

    2nd resume

--- MyResume.js
+++ MyResume.js
@@ -73,13 +73,8 @@ function resume1(resumeDiv){
 
     let skills = document.getElementById('skills');
     
-    for( let i=0 ; i<skills.children.length ; i++ ){
-        if(skills.children[i].checked){
-            let li1 = document.createElement('li');
-            li1.innerText = skills.children[i].value;
-            skillList.appendChild(li1);
-        }
-    }
+    addSkills(skills, skillList);
+
     skill.appendChild(skillList);
     resumeDiv.appendChild(skill);
 
@@ -92,20 +87,8 @@ function resume1(resumeDiv){
     let userExperience = document.getElementById('options').value;
     let list = document.createElement('ul');
     
-    if( userExperience=="Fresher" ){
-        let li = document.createElement('li');
-        li.innerText = "Fresher";
-        list.appendChild(li);
-    }
-    else {
-        let data = document.getElementsByClassName('pastExperience');
-        for( let e of data ){
-            let li = document.createElement('li');
-            li.innerText = e.value;
-            list.appendChild(li);
-            
-        }
-    }
+    expFunction(userExperience, list);
+    
     exp.appendChild(list);
     resumeDiv.appendChild(exp);
 
@@ -124,4 +107,119 @@ function resume1(resumeDiv){
     edDesc.appendChild(two);
     education.appendChild(edDesc);
     resumeDiv.appendChild(education);
+}
+
+function addSkills(skills, skillList){
+    for( let i=0 ; i<skills.children.length ; i++ ){
+        if(skills.children[i].checked){
+            let li1 = document.createElement('li');
+            li1.innerText = skills.children[i].value;
+            skillList.appendChild(li1);
+        }
+    }
+}
+
+function expFunction(userExperience, list){
+    if( userExperience=="Fresher" ){
+        let li = document.createElement('li');
+        li.innerText = "Fresher";
+        list.appendChild(li);
+    }
+    else {
+        let data = document.getElementsByClassName('pastExperience');
+        for( let e of data ){
+            let li = document.createElement('li');
+            li.innerText = e.value;
+            list.appendChild(li);
+            
+        }
+    }
+}
+
+function resume2(resumeDiv){
+    resumeDiv.setAttribute('style','background-color: white; padding: 2%; width: 65%; margin: auto;border: 1px solid gray');
+
+    // HEADER
+    let header = document.createElement('div');
+    header.setAttribute('style','display: flex; justify-content: center; align-items: center; flex-direction: column;');
+    let name = document.createElement('h1');
+    name.innerText = document.getElementById('name').value;
+    header.appendChild(name);
+
+    let add = document.createElement('p');
+    add.innerText = document.getElementById('location').value;
+    header.appendChild(add);
+
+    let no = document.createElement('p');
+    no.innerText = document.getElementById('phoneNumber').value;
+    header.appendChild(no);
+
+    let mail = document.createElement('p');
+    mail.innerText = document.getElementById('mail').value;
+    header.appendChild(mail);
+    resumeDiv.appendChild(header);
+
+    let workExpDiv = document.createElement('div');
+    let workHead = document.createElement('h3');
+    workHead.innerText = "Work Experience";
+    workExpDiv.appendChild(workHead);
+    let line = document.createElement('hr');
+    workHead.appendChild(line);
+    
+    let userInput = document.getElementById('options').value;
+    let list = document.createElement('ul');
+    
+    expFunction(userInput, list);
+    workExpDiv.appendChild(list);
+    resumeDiv.appendChild(workExpDiv);
+    
+    let educationDiv = document.createElement('div');
+    let educationHead = document.createElement('h3');
+    educationHead.innerText = "Education";
+    let line2 = document.createElement('hr');
+    educationHead.appendChild(line2);
+    educationDiv.appendChild(educationHead);
+    
+    let educationList = document.createElement('ul');
+    let master = document.getElementById('master').value;
+    if( master ){
+        let li = document.createElement('li');
+        li.innerText = "Master degree: "+master;
+        educationList.appendChild(li);
+    }
+    let bchlr = document.getElementById('bchlr').value;
+    if ( bchlr ){
+        let li = document.createElement('li');
+        li.innerText = "Bachelor degree: "+bchlr;
+        educationList.appendChild(li);
+    }
+    
+    let twelve = document.getElementById('twelve').value;
+    if ( twelve ){
+        let li = document.createElement('li');
+        li.innerText = "12th: "+twelve;
+        educationList.appendChild(li);
+    }
+
+    let tenth = document.getElementById('tenth').value;
+    if ( tenth ){
+        let li = document.createElement('li');
+        li.innerText = "10th: "+tenth;
+        educationList.appendChild(li);
+    }
+    educationDiv.appendChild(educationList);
+    resumeDiv.appendChild(educationDiv);
+
+    let skillDiv = document.createElement('div');
+    let skillHead = document.createElement('h3');
+    skillHead.innerText = "Skills";
+    let line3 = document.createElement('hr');
+    skillHead.appendChild(line3);
+    skillDiv.appendChild(skillHead);
+
+    let skillList = document.createElement('ul');
+    let skills = document.getElementById('skills');
+    addSkills(skills, skillList);
+    skillDiv.appendChild(skillList);
+    resumeDiv.appendChild(skillDiv);
 }
